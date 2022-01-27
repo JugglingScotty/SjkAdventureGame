5b5a1a26f3205d03c51188ac36fa1227e6c8ad7d
JugglingScotty <krause.scott@gmail.com>
2022-01-26 23:13:12 -0600

    Created initial main file with starting todos.

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+
+This is a basic version of the Adventure game. It is completely text-based.
+In this version of the game, users can move about through different rooms within a single setting,
+and based on the user input, it will provide descriptions for each room. This is one of the
+interesting python projects.
+
+Movement direction is crucial here – you must create walls and set the directions in which the
+users can move through the rooms, set movement restrictions, and also include a tracker that can
+track how far a user has walked or moved in the game.
+
+'''
+
+# todo Create room class.
+# todo Room instances should have values for the walls for N, S, E, W.
+# todo The value for a wall should be either a blank wall or a passage.
+# todo the room should contain something of note.
+# todo The user should provide input for choosing a direction.
+# todo upon choosing a direction, the program should create a new room.
+# todo new room creation needs to have a passage that connects back to the previous.
+# todo an option for the player should be to display the map.
+# todo rooms should contain passages that connect to existing room.

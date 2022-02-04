24ee94f4c26ac52d18fd87108665f5709d60a592
JugglingScotty <krause.scott@gmail.com>
2022-02-04 12:46:36 -0600

    Added random to get an item from the contents dictionary, fixed Pep errors.

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
+import random
+
+
 class Room:
-    #a class that represents one room in the dungeon.
-    #todo add items to the dictionary for room descriptions.
-    dict_contents = {test:"Contents",1:"First Description"}
+    # a class that represents one room in the dungeon.
+    # todo add items to the dictionary for room descriptions.
+    dict_contents = {"test": "Contents", 1: "A skull with flaming eyes."}
 
-
-
-    def __init__(self,horizontal,vertical):
+    def __init__(self, horizontal, vertical):
         self.horizontal = horizontal
         self.vertical = vertical
-        self.contents = dict_contents{1}
-        #determining the values of the corners of the rooms.
-        #a random thing in the center of the room.
+
+        # todo have this driven by the length of the dictionary
+        contents_random_int = random.randint(1, 1)
+        self.contents = Room.dict_contents[contents_random_int]
+
+        # determining the values of the sides of the rooms.
 
     def whats_in_room(self):
         return self.contents
 
+
 class Player:
-    #representing the player character during the game.
-    #the number of steps moved so far.
+    # representing the player character during the game.
+    # the number of steps moved so far.
+    pass
+
 
 class Map:
-    #representing a collection of rooms.
+    # representing a collection of rooms.
     def __init__(self):
-        #initialize a list of rooms.
+        # initialize a list of rooms.
+        pass
 
-    def addRoom(self,room):
-        #check if the room shares a wall .
-        #add the room to the end of the list.
+    def add_room(self, room):
+        # check if the room shares a wall .
+        # add the room to the end of the list.
+        pass
+
+
 '''
 
 This is a basic version of the Adventure game. It is completely text-based.
@@ -43,8 +54,8 @@ track how far a user has walked or moved in the game.
 coordinate_x = 0
 coordinate_y = 0
 
-#create a room
-newroom = Room(coordinate_x,coordinate_y)
+# create a room
+new_room = Room(coordinate_x, coordinate_y)
 
 # todo Room instances should have values for the walls for N, S, E, W.
 # todo The value for a wall should be either a blank wall or a passage.

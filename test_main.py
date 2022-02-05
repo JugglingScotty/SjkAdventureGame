f9845ace8559135389438e50713378c4901c2c1a
JugglingScotty <krause.scott@gmail.com>
2022-02-04 21:29:06 -0600

    Added my first unit test for testing the contents of a room.

--- test_main.py
+++ test_main.py
@@ -1,10 +1,14 @@
-from unittest import TestCase
+import unittest
+
+import main
 
 
-class TestRoom(TestCase):
-    pass
-
-
-class TestRoom(TestCase):
+class TestRoom(unittest.TestCase):
     def test_contents(self):
-        self.assertTrue()
+        test_room = main.Room(1, 1)
+        contents = test_room.whats_in_room()
+        self.assertEqual(contents, "A skull with flaming eyes.")
+
+
+if __name__ == '__main__':
+    unittest.main()
